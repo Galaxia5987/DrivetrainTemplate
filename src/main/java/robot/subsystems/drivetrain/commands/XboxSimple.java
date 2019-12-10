983a7f7a13fde5839d4329032eccd625a872ab6a
Adam <adam.vichik@gmail.com>
2019-12-10 17:59:11 +0200

    Create XboxSimple and set speed values

---
+++ src/main/java/robot/subsystems/drivetrain/commands/XboxSimple.java
@@ -0,0 +1,30 @@
+package robot.subsystems.drivetrain.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+
+import static robot.Constants.Drivetrain.*;
+
+public class XboxSimple extends Command {
+    private double forward = -Robot.m_robotContainer.getRightXboxY();
+    private double turn = -Robot.m_robotContainer.getLeftXboxY();
+    public XboxSimple(){
+        requires(Robot.m_drivetrain);
+    }
+
+    @Override
+    protected void initialize(){
+
+    }
+
+    @Override
+    protected void execute(){
+        Robot.m_drivetrain.setLeftSpeed(forward*RIGHT_VELOCITY + turn*LEFT_VELOCITY);
+        Robot.m_drivetrain.setRightSpeed(forward*RIGHT_VELOCITY - turn*LEFT_VELOCITY);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
