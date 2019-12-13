1fc2c3620f702d2c39b4da85e2b7792618e1551f
Adam <adam.vichik@gmail.com>
2019-12-13 13:48:13 +0200

    Create speed  Network Table

--- src/main/java/robot/subsystems/drivetrain/commands/XboxSimple.java
+++ src/main/java/robot/subsystems/drivetrain/commands/XboxSimple.java
@@ -1,26 +1,38 @@
 package robot.subsystems.drivetrain.commands;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import robot.Robot;
 
 import static robot.Constants.Drivetrain.*;
 
 public class XboxSimple extends Command {
-    private double forward = Robot.m_robotContainer.getRightXboxY();
-    private double turn = Robot.m_robotContainer.getLeftXboxX();
+    private double forward;
+    private double turn;
+    private NetworkTable xboxTable = NetworkTableInstance.getDefault().getTable("xbox");
+    private NetworkTableEntry speedEntry = xboxTable.getEntry("speed");
+
     public XboxSimple(){
         requires(Robot.m_drivetrain);
     }
 
     @Override
     protected void initialize(){
-
     }
 
     @Override
     protected void execute(){
-        Robot.m_drivetrain.setLeftSpeed(forward*RIGHT_VELOCITY + turn*LEFT_VELOCITY);
-        Robot.m_drivetrain.setRightSpeed(forward*RIGHT_VELOCITY - turn*LEFT_VELOCITY);
+        forward = Robot.m_robotContainer.getRightXboxY();
+        turn = -Robot.m_robotContainer.getLeftXboxX();
+        double leftSpeed = forward*RIGHT_VELOCITY + turn*LEFT_VELOCITY;
+        double rightSpeed = forward*RIGHT_VELOCITY - turn*LEFT_VELOCITY;
+
+        Robot.m_drivetrain.setLeftSpeed(leftSpeed);
+        Robot.m_drivetrain.setRightSpeed(rightSpeed);
+        speedEntry.setDouble(leftSpeed);
     }
 
     @Override
